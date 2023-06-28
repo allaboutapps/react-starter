411939f5783c042863398ef2b4ca701a32c02e5d
Gottfried Chen <gottfried.chen@allaboutapps.at>
2023-06-28 13:49:49 +0200

    try out action for docker verification

---
+++ scripts/check-url.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Function to check if URL is reachable with status 200
+check_url() {
+    url="$1"
+    response=$(curl --silent --head --output /dev/null --write-out "%{http_code}" "$url")
+    
+    if [[ $response -eq 200 ]]; then
+        echo "URL is reachable with status 200"
+        exit 0
+    else
+        echo "URL is not reachable or returned a different status code: $response"
+        exit 1
+    fi
+}
+
+# Usage: ./check_url.sh <url>
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <url>"
+    exit 1
+fi
+
+url="$1"
+check_url "$url"
